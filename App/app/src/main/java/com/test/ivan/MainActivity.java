319e21d7b738b32815a6dd5bb201b3740c488008
Ivan Smolyar <smolyarivan9@gmail.com>
2016-11-08 12:34:24 +0300

    added output

--- App/app/src/main/java/com/test/ivan/MainActivity.java
+++ App/app/src/main/java/com/test/ivan/MainActivity.java
@@ -4,26 +4,17 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.util.SparseIntArray;
-import android.widget.Toast;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.BitSet;
-import java.util.Date;
 import java.util.List;
-import java.util.Locale;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy.MM.dd_HH.mm.ss", Locale.ENGLISH);
+    private static final int RANGE = 100000;
 
-    private static final int RANGE = 10;
-
-    private static final int SIZE = 10;
+    private static final int SIZE = 250000;
 
     private static final int MAX = RANGE;
     private static final int MIN = -MAX;
@@ -41,18 +32,18 @@ public class MainActivity extends AppCompatActivity {
                 array.add(i, -r.nextInt(RANGE + 1));
             }
         }
+        Log.e("!!!", "Set " + array.size() + " items");
         //////////////////// run task1 /////////////////////////
         task1(array, array.size());
     }
 
     private void task1(List<Integer> array, int size) {
+        final long time = System.currentTimeMillis();
         int min = array.get(0);
         int max = array.get(0);
-        final long time = System.currentTimeMillis();
         SparseIntArray frequentArray = new SparseIntArray(size);
-        BitSet set = new BitSet(size * 2);
-
-        for (int pos = 0; pos < SIZE; pos++) {
+        BitSet set = new BitSet(array.size() * 2);
+        for (int pos = size - 1; pos >= 0; pos--) {
             int current = array.get(pos);
             if (current > max) {
                 max = current;
@@ -63,19 +54,17 @@ public class MainActivity extends AppCompatActivity {
             if (current >= 0) {
                 set.set(current);
             } else {
-                set.set(RANGE-current);
+                set.set(RANGE - current);
             }
 
             frequentArray.put(current, frequentArray.get(current) + 1);
         }
-        
-        StringBuilder b2 = new StringBuilder();
-        for (int i = 0; i <= set.length()-1; i++) {
-            b2.append(set.get(i) ? "1 " : "0 ");
-        }
-        Log.e("!!!", "Array " + array.toString() + " - " + array.size() + " items");
-        Log.e("!!!", "set " + b2.toString() + " " + set.length() + " " + set.toString());
+        long took = System.currentTimeMillis() - time;
+        Log.e("TASK1", "It took " + took + " ms");
+        //printRes(max, min, frequentArray, set);
+    }
 
+    private void printRes(int max, int min, SparseIntArray frequentArray, BitSet set) {
         for (int current = max; current >= min; current--) {
             final int times = frequentArray.get(current);
             if (times >= 2) {
@@ -83,58 +72,17 @@ public class MainActivity extends AppCompatActivity {
                 Log.e("!!!", line4);
             }
         }
-        long took = System.currentTimeMillis() - time;
-        Log.e("TASK1", "It took " + took + " ms");
-    }
-
-    private void printResult(List<Integer> array, int min, int max, SparseIntArray missing, SparseIntArray frequentArray, long took) {
-        final String line0 = "Array " + array.toString() + " - " + array.size() + " items";
-        final String line1 = "Range is " + min + " to " + max;
-        final String line2 = "Missing Numbers: " + missing;
-        final String line3 = "Duplicate Numbers: " + frequentArray.toString();
-        final String line5 = "It took " + took + " ms";
-
-        Log.e("TASK1", line0);
-        Log.e("TASK1", line1);
-        Log.e("TASK1", line2);
-        Log.e("TASK1", line3);
-        Log.e("TASK1", line5);
-
-        final File file = new File("/storage/emulated/0/task1_" + DATE_FORMAT.format(new Date()) + ".txt");
-        try {
-            file.createNewFile();
-        } catch (IOException e) {
-            Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
-        }
-        FileWriter writer = null;
-        try {
-            writer = new FileWriter(file);
-            writer.append(line0);
-            writer.append("\n");
-            writer.append(line1);
-            writer.append("\n");
-            writer.append(line2);
-            writer.append("\n");
-            writer.append(line3);
-            writer.append("\n");
-
-            for (int current = max; current >= min; current--) {
-                final int times = frequentArray.get(current);
-                if (times >= 2) {
-                    final String line4 = current + " appears " + times + " times";
-                    Log.e("TASK1", line4);
-                    writer.append(line4);
-                    writer.append("\n");
+        for (int current = MAX; current >= MIN; current--) {
+            if (current >= 0) {
+                if (!set.get(current)) {
+                    Log.e("!!!","missing " + current);
+                }
+            } else {
+                if (!set.get(RANGE-current)) {
+                    Log.e("!!!","missing " + current);
                 }
             }
-
-            writer.append(line5);
-            writer.append("\n");
-
-            writer.flush();
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
+
 }
