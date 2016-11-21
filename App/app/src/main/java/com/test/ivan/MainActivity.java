1da5e0d3c6a4a63fc8c7d26398fe23d9589c76af
Ivan Smolyar <smolyarivan9@gmail.com>
2016-11-21 12:21:44 +0300

    output fixed

--- App/app/src/main/java/com/test/ivan/MainActivity.java
+++ App/app/src/main/java/com/test/ivan/MainActivity.java
@@ -3,18 +3,14 @@ package com.test.ivan;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.util.SparseIntArray;
 
-import java.util.ArrayList;
-import java.util.BitSet;
-import java.util.List;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int RANGE = 100000;
 
-    private static final int SIZE = 250000;
+    private static final int SIZE = 1000000;
 
     private static final int MAX = RANGE;
     private static final int MIN = -MAX;
@@ -23,28 +19,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ///////////////////////// init /////////////////////
-        List<Integer> array = new ArrayList<>(SIZE);
+        int[] array = new int[SIZE];
         Random r = new Random();
         for (int i = 0; i < SIZE; i++) {
             if (i % 2 == 0) {
-                array.add(i, r.nextInt(RANGE + 1));
+                array[i] = r.nextInt(RANGE + 1);
             } else {
-                array.add(i, -r.nextInt(RANGE + 1));
+                array[i] = -r.nextInt(RANGE + 1);
             }
         }
-        Log.e("!!!", "Set " + array.size() + " items");
+        Log.e("!!!", "Set " +   array.length + " items");
         //////////////////// run task1 /////////////////////////
-        task1(array, array.size());
+        task1(array, array.length);
     }
 
-    private void task1(List<Integer> array, int size) {
+    private void task1(int[] array, int size) {
         final long time = System.currentTimeMillis();
-        int min = array.get(0);
-        int max = array.get(0);
-        SparseIntArray frequentArray = new SparseIntArray(size);
-        BitSet set = new BitSet(array.size() * 2);
+        int min = array[0];
+        int max = array[0];
+
+        int[] frequent = new int[RANGE * 2];
+
         for (int pos = size - 1; pos >= 0; pos--) {
-            int current = array.get(pos);
+            int current = array[pos];
             if (current > max) {
                 max = current;
             } else if (current < min) {
@@ -52,35 +49,25 @@ public class MainActivity extends AppCompatActivity {
             }
 
             if (current >= 0) {
-                set.set(current);
+                frequent[current] = frequent[current] + 1;
             } else {
-                set.set(RANGE - current);
+                frequent[RANGE - current - 1] = frequent[RANGE - current - 1] + 1;
             }
-
-            frequentArray.put(current, frequentArray.get(current) + 1);
         }
         long took = System.currentTimeMillis() - time;
         Log.e("TASK1", "It took " + took + " ms");
-        //printRes(max, min, frequentArray, set);
+        Log.e("!!!", "min " + min + ", max " + max);
+        //printRes(frequent);
     }
 
-    private void printRes(int max, int min, SparseIntArray frequentArray, BitSet set) {
-        for (int current = max; current >= min; current--) {
-            final int times = frequentArray.get(current);
+    private void printRes(int[] frequent) {
+        for (int current = frequent.length - 1; current >= 0; --current) {
+            final int times = frequent[current];
             if (times >= 2) {
-                final String line4 = current + " appears " + times + " times";
-                Log.e("!!!", line4);
-            }
-        }
-        for (int current = MAX; current >= MIN; current--) {
-            if (current >= 0) {
-                if (!set.get(current)) {
-                    Log.e("!!!","missing " + current);
-                }
-            } else {
-                if (!set.get(RANGE-current)) {
-                    Log.e("!!!","missing " + current);
-                }
+                final String line5 = (current > RANGE ? (-(current % RANGE + 1)) : current) + " appears " + times + " times";
+                Log.e("!!!", line5);
+            } else if (times == 0) {
+                Log.e("!!!", (current > RANGE ? (-(current % RANGE + 1)) : current) + " missing");
             }
         }
     }
